d85d192cabf226feef607833cce20776ad51c2ef
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-11-09 14:28:51 +0530

    my program

---
+++ ArithmeticOperations.java
@@ -0,0 +1,19 @@
+public class  ArithmeticOperations
+{
+   public static void main(String[]args){
+
+int a = 30;
+int b = 20;
+
+
+
+int sum1 = a + b;
+System.out.println("Addition:"+sum1);
+int sum2 = a - b;
+System.out .println("subraction:"+sum2);
+int sum3 = a * b;
+System.out.println("Multipletion:"+sum3);
+int sum4 = a / b;
+System.out.println("Division:"+sum4);
+   }
+}
