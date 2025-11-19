1b373fc814fd18e53cb02e252ad73c146d3fb3eb
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-11-19 21:23:13 +0530

    we are the programe addition

---
+++ Addition.java
@@ -0,0 +1,23 @@
+public class Addition  
+{
+	public static void main(String[] args) 
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+
+
+
+          int sum1 = a+b;
+		System.out.println("sum :"+sum1);
+		int sum2 = a*b;
+		System.out.println("sum :"+sum2);
+		int sum3 = a/b;
+		System.out.println("sum :"+sum3);
+		int sum4 = a%b;
+		System.out.println("sum :"+sum4);
+	}
+}
+
+    
+
