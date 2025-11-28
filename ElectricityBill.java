e5d2680e69f85f751ef71329754bfdc21c1acdfa
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-11-28 23:14:23 +0530

    there are best programe is using

---
+++ ElectricityBill.java
@@ -0,0 +1,22 @@
+public class ElectricityBill
+
+{
+	public static void main(String[]args)
+	{
+		int unit =Integer.parseInt(args[0]);
+		int Rs= Integer.parseInt(args[1]);
+		double Bill=unit+Rs;
+
+
+
+
+		System.out.println("unit"+unit);
+		System.out.println("Rs"+Rs);
+		System.out.println("Bill"+Bill);
+
+
+	}
+
+
+
+}
