4f0fa8d72dccd66bb0a89b4d502f2ef4b35d63fe
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-11-28 23:43:50 +0530

    where is best is programe is using is prorame

---
+++ FactorialNumber.java
@@ -0,0 +1,19 @@
+public class FactorialNumber
+
+{
+	public static void main(String[]args)
+	{
+		int n = Integer.parseInt(args[0]);
+		 fact = 1;
+		 
+		 
+			 for(int i=1;i<=n;i++)
+		      {
+				 fact = fact*i;
+
+
+		}
+		System.out.println("Factorial =" +(Fact));
+
+}
+}
