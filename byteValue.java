f1ca0d68a3ea240449055011d124153b55f8b8c0
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-12-03 00:12:20 +0530

    is using is programe

---
+++ byteValue.java
@@ -0,0 +1,23 @@
+public class byteValue {
+   public byteValue() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("\n Byte range:");
+      System.out.println("min:-128");
+      System.out.println("mix:127");
+      System.out.println("size:8");
+      System.out.println("\n Long range:");
+      System.out.println("min:-9223372036854775808");
+      System.out.println("mix:9223372036854775807");
+      System.out.println("size:64");
+      System.out.println("\n Short range:");
+      System.out.println("min:-32768");
+      System.out.println("mix:32767");
+      System.out.println("size:16");
+      System.out.println("\n Integer range:");
+      System.out.println("min:-2147483648");
+      System.out.println("mix:2147483647");
+      System.out.println("size:32");
+   }
+}
