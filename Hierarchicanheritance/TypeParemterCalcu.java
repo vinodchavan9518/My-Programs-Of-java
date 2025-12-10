fedea741ed2120b8518a937193811e720486c01e
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-12-10 21:26:16 +0530

    there is basical programe

---
+++ Hierarchicanheritance/TypeParemterCalcu.java
@@ -0,0 +1,37 @@
+package Hierarchicanheritance;
+
+
+    class Calcul<T>
+{
+	int sum(int x,int y)
+	{
+		return (x+y);
+	}
+	
+}
+
+
+
+public class TypeParemterCalcu {
+
+	public static void main(String[] args) {
+		
+		Calcul<Integer>intType=new Calcul<Integer>();
+		System.out.println("sum addition is:"+intType.sum(12, 12));
+		
+		
+		Calcul<Double>doubleType=new Calcul<>();
+		System.out.println("Double is Sum:"+doubleType.sum(1, 2));
+		
+		Calcul<Long>longType=new Calcul<Long>();
+		System.out.println("Long Number :"+longType.sum(23, 12));
+		
+
+	}
+
+}
+
+
+
+    
+}
