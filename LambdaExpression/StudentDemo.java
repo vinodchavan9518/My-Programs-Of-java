3375eebd7ca9f64e32fe7fb5ee7f5ad58dfb0fac
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-12-12 10:34:23 +0530

    seiron baseal programe

---
+++ LambdaExpression/StudentDemo.java
@@ -0,0 +1,41 @@
+package LambdaExpression;
+
+import java.util.function.Supplier;
+
+
+    class Student
+{ 
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+ 
+    public Student(int id, String name, String gender, int age) 
+	{
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+ 
+    @Override
+    public String toString() 
+		{
+        return "[id=" + id + ", name=" + name + ", gender=" + gender + ", age=" + age + "]";
+        }
+}
+		
+public class StudentDemo 
+
+{
+	public static void main(String[] args) 
+	{
+      Supplier<Student> stdSupplier = () -> new Student(1, "Virat", "Male", 24);
+		
+        Student std =  stdSupplier.get();
+        System.out.println(std);       
+	}
+}
+
+
+}
