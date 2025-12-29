edc35739e82560e7103fbe0c5172ddc4e7ff2925
vinodchavan9518 <vinodchavan9518@gmail.com>
2025-12-29 22:11:20 +0530

    find is program basical program

---
+++ ExecptionHandling/ExecptionMultipleOverriding.java
@@ -0,0 +1,48 @@
+package ExecptionHandling;
+
+public class ExecptionMultipleOverriding {
+    
+	public static void main(String[] args) {
+		String str = IO.readln("Enter a class name : ");
+	    ParentClass pr = new ChildClass();
+	   try {
+		pr.loadingClass(str);
+		IO.println("Class "+str+" loaded successfully.");
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	}
+	
+
+}
+class ParentClass{
+	
+	public void loadingClass(String className) throws ClassNotFoundException {
+		
+		Class.forName(className);
+	}
+}
+class ChildClass extends ParentClass{
+	@Override
+public void loadingClass(String className) throws ClassNotFoundException {
+		
+	try {
+      if(className.isEmpty() || className == null) {
+    	  throw new IllegalArgumentException();
+      }
+      else {
+    	  super.loadingClass(className);
+      }
+	}
+	catch (IllegalArgumentException e) {
+		e.printStackTrace();
+	}
+	}
+
+
+	}
+
+
+
+
