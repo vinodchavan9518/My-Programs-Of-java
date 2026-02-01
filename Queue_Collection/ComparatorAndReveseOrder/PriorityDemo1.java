415328489be3daf5a90b65f7963a51b210e3a408
vinodchavan9518 <vinodchavan9518@gmail.com>
2026-02-01 14:45:20 +0530

    find the menu driven program to perform various operations on PriorityQueue

---
+++ Queue_Collection/ComparatorAndReveseOrder/PriorityDemo1.java
@@ -0,0 +1,80 @@
+package Queue_Collection.ComparatorAndReveseOrder;
+
+import java.util.PriorityQueue;
+
+//find the menu driven program to perform various operations on PriorityQueue
+public class PriorityDemo1 {
+    void main()
+	{
+		PriorityQueue<Integer> pq=new PriorityQueue<>();
+		
+		int choice;
+		do
+		{
+			IO.println("\n====PriorityQueue Menu==============");
+			IO.println("1.Insert element");
+			IO.println("2.Remove head element (poll");
+			IO.println("3.Veiw head element (peek)");
+			IO.println("4.Dispaly priorityQueue");
+			IO.println("5.Exit");
+			choice=Integer.parseInt(IO.readln("Enter your choice:"));
+			
+			switch(choice)
+			{
+			case 1->
+			{
+			
+				int val=Integer.parseInt(IO.readln("Enter element to insert:"));
+                 pq.add(val);
+                 IO.println(val+"inserted");
+             }
+			
+			case 2->
+			{
+				if(pq.isEmpty())
+				{
+					IO.println("PriorityQueue is empty");
+				}
+				else
+				{
+					IO.println("Removed:"+pq.poll());
+				}
+			}
+			case 3->
+			{
+				if(pq.isEmpty())
+				{
+					IO.println("PriorityQueue: is empty");
+					
+				}
+				else
+				{
+					IO.println("Head elemet :"+pq.peek());
+					
+				}
+			}
+			
+			case 4->
+			{
+				IO.println("PriorityQueue:"+pq);
+			
+			}
+			case 5->
+			{
+				IO.println("Exiting...");
+			}
+			default->
+			{
+				IO.println("Invalid choice");
+			}
+			}
+		}
+			
+			
+		while(choice !=5);
+			
+		}
+	}
+
+    
+
