89c4ba6f9c60b5ea7f7f1943cb73a0c06e63431a
vinodchavan9518 <vinodchavan9518@gmail.com>
2026-02-17 23:10:41 +0530

    find the method reference for the following code

---
+++ Java8Features/Default&StaticMethodInterface/DefaultAndConcrteMehod.java
@@ -0,0 +1,37 @@
+package Java8Features;
+
+//find the method reference for the following code
+
+
+interface Dog
+{
+	default void show()
+	{
+		System.out.println("default show interface method A");
+	}
+}
+class Cate
+{
+	public  void show() //second private 
+	{
+		System.out.println("Concreate show method of class B ");
+	}
+}
+//class Dog1 extends Cate implements Dog//first private
+//{
+	//public  void show ()
+	//{
+		//System.out.println("Dog is running is mehod ");
+	//}
+//}
+public class DefaultAndConcrteMehod {
+
+	public static void main(String[] args) {
+		
+		Cate dog =new Cate();
+				dog.show();
+		
+
+	}
+
+}
