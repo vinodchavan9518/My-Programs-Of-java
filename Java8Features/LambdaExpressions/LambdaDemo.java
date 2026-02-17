e7e420929ce41de32d7328ce81daae7d0a1537d7
vinodchavan9518 <vinodchavan9518@gmail.com>
2026-02-17 23:51:10 +0530

    find the lambda expression in the below code and explain it

---
+++ Java8Features/LambdaExpressions/LambdaDemo.java
@@ -0,0 +1,13 @@
+package Java8Features.LambdaExpressions;
+//find the lambda expression in the below code and explain it
+public class LambdaDemo {
+    public static void main(String[] args) {
+        // Lambda expression to implement Runnable interface
+        Runnable runnable = () -> System.out.println("Hello from a lambda expression!");
+        
+        // Running the thread
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+    
+}
